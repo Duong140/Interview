a644da4d39389ddb1924ac15340ac8909a77324d
Duong140 <doung140@gmail.com>
2025-01-16 11:11:37 -0500

    Interview

--- NewProject/src/test/java/com/Interview/java/ExcelCode.java
+++ NewProject/src/test/java/com/Interview/java/ExcelCode.java
@@ -2,6 +2,7 @@ package com.Interview.java;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -19,12 +20,37 @@ public class ExcelCode {
 
 
 	public static void main(String[] args) throws IOException{
-	//heyyyasda
+	
+		String name = "Mike";
+		String name1 = "I K Ef m";
+		System.out.println(findAnagrams(name,name1));
 	}
 	
 	
-
-	
+		
+	public static boolean findAnagrams(String str1, String str2) {
+		//git rid of all whitespaces and convert to lower case
+		str1 = str1.replaceAll("\\s", "").toLowerCase();
+		str2 = str2.replaceAll("\\s", "").toLowerCase();
+		
+		
+		//check if the strings are the same size if not return false
+		if(str1.length() != str2.length()) {
+			return false;
+		}
+		
+		
+		//convert strigns to char arrays
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+		
+		//sort the array
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+		
+		return Arrays.equals(arr1, arr2);
+		
+	}
 
 }
 
