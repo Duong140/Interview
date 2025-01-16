779aa60a48511f2b5d016f5f795c77904737f67a
Duong140 <doung140@gmail.com>
2025-01-16 12:05:47 -0500

    empty

--- NewProject/src/test/java/com/Interview/java/ExcelCode.java
+++ NewProject/src/test/java/com/Interview/java/ExcelCode.java
@@ -20,24 +20,15 @@ public class ExcelCode {
 
 
 	public static void main(String[] args){
-	String name = "Mike";
-		System.out.println(reverseString(name));
 	
-	}
 	
-	public static String reverseString(String str) {
-		//create empty string to store new string
-		String rev = "";
-		
-		for(int i = str.length()-1; i >= 0; i--) {
-			rev += str.charAt(i);
-		}
-		
-		return rev;
 	}
 	
 	
 	
+	public static void reverseSentence(String str) {
+		//put input into a string array
+	}
 
 }
 
